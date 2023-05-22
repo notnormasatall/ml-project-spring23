d778ac657112e3b44a5a2abc842794cef27dcc85
notnormasatall <taras.rodzin@ukr.net>
2023-05-22 13:01:39 +0300

    Minor changes to UI

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import tempfile
 import subprocess
 import data_utils
 import model_utils
+import pretty_midi
 
 MODEL_PATH = "linear_norm_t5.ckpt"
 model = model_utils.SimpleModel.load_from_checkpoint(MODEL_PATH)
@@ -31,9 +32,8 @@ async def upload_file(request: Request, file: UploadFile = File(...)):
 
     # Pass the data to your model and obtain the MIDI output
     tokens = model_utils.predict_tokens(
-        model, None, mask, input, None)
+        model, output, mask, input, output_mask)
 
-    print(tokens)
     # Return the MIDI file path for the template
     midi_file = "MIDI-Unprocessed_02_R2_2008_01-05_ORIG_MID--AUDIO_02_R2_2008_wav--4.midi"
     # data_utils.generate_midi_file(
@@ -41,9 +41,8 @@ async def upload_file(request: Request, file: UploadFile = File(...)):
     return templates.TemplateResponse("index.html", {"request": request, "midi_file": midi_file})
 
 
-@app.get("/midi/{filename}")
+@app.get("/{filename}")
 def download_midi(filename: str):
-    print()
     return FileResponse(filename, media_type='audio/midi')
 
 
