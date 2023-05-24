a22a11700e501326370a23a65dc67d322affc6af
notnormasatall <taras.rodzin@ukr.net>
2023-05-24 11:20:21 +0300

    'returnget_midi

--- data_utils.py
+++ data_utils.py
@@ -33,7 +33,7 @@ def get_spectogram(audio, sr=SAMPLE_RATE, hl=HOP_WIDTH, mn=MEL_BINS, n_fft=N_FFT
                                           hop_length=hl, n_mels=mn).T
 
 
-def pad_spectrogram(spectrogram, seq_size=511, log_mel_bins = 512):
+def pad_spectrogram(spectrogram, seq_size=511, log_mel_bins=512):
     '''
     Splits spectogram into batches, each having a mask.
     '''
@@ -43,16 +43,19 @@ def pad_spectrogram(spectrogram, seq_size=511, log_mel_bins = 512):
     for i in range(0, spectrogram.shape[0], seq_size):
 
         if i+seq_size > spectrogram.shape[0]:
-            result = np.append(spectrogram[i:i+seq_size], 
-                               np.array([1 for _ in range(log_mel_bins)]).reshape(1, log_mel_bins), axis=0)      
-            batches.append(np.append(result, 
+            result = np.append(spectrogram[i:i+seq_size],
+                               np.array([1 for _ in range(log_mel_bins)]).reshape(1, log_mel_bins), axis=0)
+            batches.append(np.append(result,
                                      np.array([0 for _ in range((i+seq_size - spectrogram.shape[0])*(log_mel_bins))]).reshape(i+seq_size - spectrogram.shape[0], log_mel_bins), axis=0))
-            attention_mask.append([1 for _ in range(spectrogram.shape[0] - i + 1)] + [0 for _ in range(i+seq_size - spectrogram.shape[0])])
+            attention_mask.append([1 for _ in range(spectrogram.shape[0] - i + 1)] + [
+                                  0 for _ in range(i+seq_size - spectrogram.shape[0])])
             continue
-        batches.append(np.append(spectrogram[i:i+seq_size], np.array([1 for _ in range(log_mel_bins)]).reshape(1, log_mel_bins), axis=0))
+        batches.append(np.append(spectrogram[i:i+seq_size], np.array(
+            [1 for _ in range(log_mel_bins)]).reshape(1, log_mel_bins), axis=0))
         attention_mask.append([1 for _ in range(seq_size+1)])
-    
-    batches, attention_mask = np.array(batches).reshape(len(batches), seq_size + 1, log_mel_bins), np.array(attention_mask).reshape(len(attention_mask), seq_size + 1)
+
+    batches, attention_mask = np.array(batches).reshape(len(
+        batches), seq_size + 1, log_mel_bins), np.array(attention_mask).reshape(len(attention_mask), seq_size + 1)
     batches = batches.astype(np.float32)
     attention_mask = attention_mask.astype(np.float32)
     return batches, attention_mask
@@ -176,7 +179,8 @@ def generate_midi_file(predicted_tokens, path="", max_seq_len=4088):
                     abs_time, curr_vel)
             elif "NoteOff" in detokenizer_vocab[token]:
                 try:
-                    start, vel = queue[int(detokenizer_vocab[token].split("_")[1])]
+                    start, vel = queue[int(
+                        detokenizer_vocab[token].split("_")[1])]
                     pitch = int(detokenizer_vocab[token].split("_")[1])
                     end = abs_time
                     piano.notes.append(pretty_midi.Note(
@@ -193,3 +197,4 @@ def generate_midi_file(predicted_tokens, path="", max_seq_len=4088):
 
     mid.instruments.append(piano)
     mid.write(path)
+    return path
